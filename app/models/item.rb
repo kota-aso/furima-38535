18448a08cecb26d4009fbb400ed6af1ce6f39f7f
kota-aso <kotaaso1212@gmail.com>
2022-11-09 22:17:50 +0900

    ActiveHashの導入とアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_status
+  belongs_to :shipping_charger
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
+  validates :item_name, :item_description, :price, :user, : presence: true
+
+  validates :item_category, :item_status, :shipping_charger, :prefecture, :days_to_ship,  numericality: { other_than: 1 } 
+
+
 end
