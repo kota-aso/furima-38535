5d75caf2dd2f904f128d1af2bff97eb3991d1e50
kota-aso <kotaaso1212@gmail.com>
2022-11-10 17:25:56 +0900

    itemモデルのバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,10 +6,14 @@ class Item < ApplicationRecord
   belongs_to :shipping_charger
   belongs_to :prefecture
   belongs_to :days_to_ship
+  has_one_attached :image
 
-  validates :item_name, :item_description, :price, :user,  presence: true
 
-  validates :item_category, :item_status, :shipping_charger, :prefecture, :days_to_ship,  numericality: { other_than: 1 } 
+  validates :item_name, :item_description, :user_id, :image,  presence: true
+
+  validates :price, presence: true, numericality: {in: 300..9999999}
+
+  validates :item_category_id, :item_status_id, :shipping_charger_id, :prefecture_id, :days_to_ship_id,  numericality: { other_than: 1 } 
 
 
 end
