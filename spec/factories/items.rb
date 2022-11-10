3ba9d9c9b56339b4af31a0f1903d51e34f8f085d
kota-aso <kotaaso1212@gmail.com>
2022-11-10 21:13:39 +0900

    fctorybotの導入

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name {'test'}
+    item_description {'testtesttest'}
+    item_category_id {'2'}
+    item_status_id {'2'}
+    shipping_charger_id {'2'}
+    prefecture_id {'2'}
+    days_to_ship_id {'2'}
+    price {'1000'}
+    user_id {'1'}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
