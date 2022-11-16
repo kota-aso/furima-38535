d2f5d406f4ef4733d5b2ea9c5cc0f8a86d6d3bf8
kota-aso <kotaaso1212@gmail.com>
2022-11-16 15:27:10 +0900

    tokenバリデーションの実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :street, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :street, :building, :phone_number, :user_id, :item_id, :token
 
   validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
@@ -9,6 +9,8 @@ class OrderAddress
   validates :phone_number, presence: true, format: {with: /\A\d{10}\z|\A\d{11}\z/}
   validates :user_id, presence: true
   validates :item_id, presence: true
+  validates :token, presence: true
+
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
