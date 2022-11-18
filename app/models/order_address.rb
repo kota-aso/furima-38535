15ff394da9d202dfae6514757d8ad5345ec1a585
kota-aso <kotaaso1212@gmail.com>
2022-11-18 11:42:08 +0900

    エラーメッセージ の修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,7 +2,7 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :street, :building, :phone_number, :user_id, :item_id, :token
 
-  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "にハイフン（-）を含んでください"}
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
   validates :city, presence: true
   validates :street, presence: true
